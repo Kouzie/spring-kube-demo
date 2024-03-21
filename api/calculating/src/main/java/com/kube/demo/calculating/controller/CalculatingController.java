be9608a105e8b6d24c453b6b465a189ae77804f0
Giyong Ko <kgy1996@naver.com>
2024-03-21 10:06:00 +0900

    add poison version for greeting

--- api/calculating/src/main/java/com/kube/demo/calculating/controller/CalculatingController.java
+++ api/calculating/src/main/java/com/kube/demo/calculating/controller/CalculatingController.java
@@ -1,7 +1,6 @@
 package com.kube.demo.calculating.controller;
 
 import com.kube.demo.calculating.config.CalculatingConfiguration;
-import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @Slf4j
 @RestController
@@ -18,7 +19,9 @@ public class CalculatingController {
 
     @Value("${demo}")
     private String demo;
-    private final MeterRegistry registry;
+
+    @Value("${is.poison}")
+    private Boolean poison;
 
     private final CalculatingConfiguration config;
 
@@ -31,10 +34,22 @@ public class CalculatingController {
     @GetMapping("/{num1}/{num2}")
     public Long addNumbers(@PathVariable Long num1, @PathVariable Long num2) {
         log.info("addNumbers invoked, num1:{}, num2:{}", num1, num2);
-        registry.counter("item.num", "num", num1.toString()).increment();
-        registry.counter("item.num", "num", num2.toString()).increment();
+        if (poison) return poison(num1, num2);
+        else return num1 + num2;
+    }
+
+    Random random = new Random();
+
+    private Long poison(Long num1, Long num2) {
+        // 0과 1 사이의 무작위 수 생성
+        double chance = random.nextDouble();
+        // 30% 확률로 실패로 간주
+        if (chance < 0.3) {
+            throw new IllegalArgumentException("poison!!");
+        }
         Long result = num1 + num2;
         return result;
     }
+
 }
 
--- api/greeting/src/main/java/com/kube/demo/greeting/contorller/GreetingController.java
+++ api/greeting/src/main/java/com/kube/demo/greeting/contorller/GreetingController.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kube.demo.common.HelloJava;
 import com.kube.demo.greeting.client.CalculatingClient;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
+import io.micrometer.core.instrument.*;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @RestController
@@ -29,26 +29,55 @@ public class GreetingController {
     @Value("${greeting.message}")
     private String greetingMessage;
 
-    private final Meter meter;
-    private LongCounter counter;
-    private Attributes attributes;
+    // PrometheusMeterRegistry
+    // prometheus micrometer exporter
+    private final MeterRegistry registry;
+    private Counter counter;
+    private Gauge gauge;
+    private AtomicLong result = new AtomicLong(0);
+    private DistributionSummary summary;
+    private Timer timer;
 
     @PostConstruct
     private void init() {
-        // Build counter e.g. LongCounter
-        this.counter = meter
-                .counterBuilder("processed_jobs")
-                .setDescription("Processed jobs")
-                .setUnit("1")
-                .build();
-        this.attributes = Attributes.of(AttributeKey.stringKey("Key"), "SomeWork");
+        // Counter 설정
+        this.counter = Counter.builder("api.call.count")
+                .description("api call count")
+                .tags("team", "monitoring", "deploy_version", "dev")
+                .register(registry);
+
+        // Gauge 설정
+        this.gauge = Gauge.builder("result.sum", this.result, AtomicLong::get)
+                .description("result sum")
+                .register(registry);
+
+        // summary
+        this.summary = DistributionSummary.builder("request.num.size")
+                .baseUnit("num")
+                .register(registry);
+
+        // timer
+        this.timer = Timer
+                .builder("test.timer")
+                .tags("team", "monitoring", "deploy_version", "dev")
+                .publishPercentiles(0.8, 0.9, 1.0)
+                .publishPercentileHistogram()
+                .serviceLevelObjectives(
+                        Duration.ofMillis(200), // 200ms
+                        Duration.ofMillis(400), // 400ms
+                        Duration.ofMillis(600), // 600ms
+                        Duration.ofMillis(800), // 800ms
+                        Duration.ofMillis(1000) // 1000ms
+                )
+                .register(registry);
+
     }
+
 
     @GetMapping
     public String greet() throws JsonProcessingException {
         log.info("greet invoked");
-        // meter.counterBuilder("item.num", "num", "1").increment();
-        counter.add(1, attributes);
+        counter.increment(1);
         HelloJava helloJava = new HelloJava(greetingMessage, LocalDateTime.now());
         return objectMapper.writeValueAsString(helloJava);
     }
@@ -56,7 +85,38 @@ public class GreetingController {
     @GetMapping("/{num1}/{num2}")
     public String calculate(@PathVariable Long num1, @PathVariable Long num2) {
         log.info("calculate invoked, num1:{}, num2:{}", num1, num2);
-        return calculatingClient.addNumbers(num1, num2).toString();
+        Long addResult = calculatingClient.addNumbers(num1, num2);
+        // 결과 값을 저장할 AtomicInteger 생성
+        result.set(addResult);
+        summary.record(num1);
+        summary.record(num2);
+        return result.toString();
+    }
+
+    @GetMapping("/summary")
+    public String summary() {
+        String result = "";
+        result += "Total count: " + summary.count() + "\n";
+        result += "Total sum: " + summary.totalAmount() + "\n";
+        result += "Average: " + summary.mean() + "\n";
+        result += "Maximum: " + summary.max() + "\n";
+        return result;
+    }
+
+    @GetMapping("/record")
+    public String record() throws InterruptedException {
+        // 작업 시작 전 시간 측정
+        long startTime = System.nanoTime();
+        Thread.sleep(1000); // 측정할 이벤트
+        long endTime = System.nanoTime();
+        timer.record(endTime - startTime, TimeUnit.NANOSECONDS);
+
+        // 작업 수행 후 Timer에서 정보 가져오기
+        String result = "";
+        result += "총 실행 횟수: " + timer.count() + "\n";
+        result += "평균 실행 시간(ms): " + timer.mean(TimeUnit.MILLISECONDS) + "\n";
+        result += "최대 실행 시간(ms): " + timer.max(TimeUnit.MILLISECONDS) + "\n";
+        result += "총 실행 시간(ms): " + timer.totalTime(TimeUnit.MILLISECONDS) + "\n";
+        return result;
     }
 }
-
